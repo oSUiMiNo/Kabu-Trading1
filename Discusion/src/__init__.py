e7ef3b6b4b7b2f6b8a43e16ac146bbf6e84fc843
中嶋彩乃 <h170b029@gunma-u.ac.jp>
2026-02-03 13:48:06 +0900

    .gitignoreファイルを更新し、エージェントユーティリティを追加
    - .gitignoreにPython、仮想環境、IDE、OS関連の設定を追加
    - エージェントの機能を実装するためのユーティリティ関数を追加
    - call_agent関数をsrcパッケージに統合

--- Discusion/src/__init__.py
+++ Discusion/src/__init__.py
@@ -1,8 +1,6 @@
 """
 株銘柄評価ツール - srcパッケージ
 """
-from .file_to_stream import file_to_stream
-from .utils import call_agent
-from .print_file import print_file
+from AgentUtil import call_agent
 
-__all__ = ["file_to_stream", "call_agent", "print_file"]
+__all__ = ["call_agent"]
